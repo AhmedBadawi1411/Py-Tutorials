f34d23c56d902a78513eb1dec7e68999570de8fb
Ahmed Badawi <ahmedbadawi.1411@gmail.com>
2025-06-20 06:17:35 +0300

    syntax

---
+++ syntax.py
@@ -0,0 +1,13 @@
+# @param self: refers to current instance;
+# fun: __init__(): dunder method || constractor method
+
+class Member:
+   def __init__(self):
+       print("GG")
+       pass
+   
+member = Member()
+Member()
+Member()
+
+print(member)
